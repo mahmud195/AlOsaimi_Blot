4de42e8ff294ef4d274a6e636d46d290e1453fe0
Mahmoud Abdelazem <83193905+mahmud195@users.noreply.github.com>
2026-02-03 10:31:34 +0200

    FF

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/AlOsaimi_Blot/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/AlOsaimi_Blot/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
